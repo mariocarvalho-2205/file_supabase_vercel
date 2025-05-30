21c7e1079347b9a882c4373907a6525decf3a657
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2025-05-30 15:26:25 -0300

    feat: alunos list ok

--- frontend/src/pages/Alunos.jsx
+++ frontend/src/pages/Alunos.jsx
@@ -3,24 +3,26 @@ import api from "../../services/api"
 import { useEffect, useState } from 'react'
 
 const Alunos = () => {
-  const { alunos, setAlunos} = useState([])
+  const [ alunos, setAlunos] = useState([])
 
   useEffect(() => {
     const fetchAlunos = async () => {
       try {
         const response = await api.get("/alunos/all")
-        setAlunos(response.data)
+        setAlunos(response.data.aluno)
+        console.log(response.data.aluno)
       } catch (error) {
         console.error("erro no fetch", error)
       }
     }
+    fetchAlunos()
   }, [])
 
   return (
     <div>
         <h1>Alunos</h1>
       <NavLink to="/">Home</NavLink>
-      {alunos && alunos.map((a) => (<p>{a.nome}</p>))}
+      {alunos.map(a => (<p key={a.id}>{a.nome}</p>))}
 
     </div>
   )
